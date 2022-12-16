aa1f259af020240f755210a6fe2c6f9dac106b2d
ali <alimoslehirad@gmail.com>
2022-12-16 19:52:41 +0330

    change django version

---
+++ weather-app/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ali/Devops/skylogs/django-statsd-monitoring/django-prometheus-via-statsd/django-metrics-prometheus/weather-app/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
